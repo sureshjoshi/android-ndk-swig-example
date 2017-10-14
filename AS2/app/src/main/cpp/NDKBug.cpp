9998092774682972a4aa398aa3af4caa9592c513
SJ <suresh@vicarasolutions.com>
2017-10-14 18:37:53 -0400

    Pulled in code from existing NDK example application and removed unused files.

--- AS2/app/src/main/cpp/NDKBug.cpp
+++
@@ -1,5 +0,0 @@
-// According to the StackOverflow post below, Android NDK can't compile a single source file, so a
-// solution is to add an empty file...
-// Yeah... Seriously... This ACTUALLY fixed my compilation error
-
-//https://stackoverflow.com/questions/22465933/no-rule-to-make-target-when-compiling-ndk-sources-on-windows-with-gradle
