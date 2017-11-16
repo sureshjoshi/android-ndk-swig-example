f2431ca6924bfb06a553fbb47dc9a1cd3172a4b5
SJ <suresh@vicarasolutions.com>
2017-11-16 03:48:37 -0500

    Re-named unit test

--- AS3/app/src/test/java/com/sureshjoshi/android/ndkexample/NativeUnitTest.kt
+++ AS3/app/src/test/java/com/sureshjoshi/android/ndkexample/NativeUnitTest.kt
@@ -1,17 +1,13 @@
 package com.sureshjoshi.android.ndkexample
 
+import com.sureshjoshi.core.SeePlusPlus
 import org.junit.Assert
 import org.junit.Test
 import java.io.File
 
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
 class NativeUnitTest {
     @Test
-    fun addition_isCorrect() {
+    fun nativeMultiply_isCorrect() {
         // TODO: Read this directory from Gradle
         val wrapperLib = File("app/.externalNativeBuild/cmake/debug/osx/libSeePlusPlus_Wrapper.jnilib")
         System.out.println(wrapperLib.absolutePath)
